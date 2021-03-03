ca746ea8f916c403f1f2270df00e6e69b9c14a7d
dietmarwo <drdietmarwolz@gmail.com>
2021-03-03 13:48:27 +0100

    v1.2.15

--- examples/interferometry.py
+++ examples/interferometry.py
@@ -2,6 +2,8 @@
 # Markus Märtens @CoolRunning and is extended here by a 
 # fcmaes parallel differential evolution solver for comparison with the pagmo island concept.
 # Tested with Anaconda 2020.11 https://repo.anaconda.com/archive/ using Python 3.8 on Linux
+# Corresponds to the equivalent python example
+# https://github.com/dietmarwo/fcmaes-java/blob/master/src/main/java/fcmaes/examples/Interferometry.java
 # The test image used is here: https://api.optimize.esa.int/data/interferometry/orion.jpg
 
 import pygmo as pg
@@ -10,7 +12,7 @@ from interferometryudp import Interferometry
 from fcmaes import de, cmaes, retry, advretry
 from fcmaes.optimizer import single_objective, de_cma_py, Cma_python, De_python, Cma_cpp
 
-udp = Interferometry(11, './img/orion.jpg', 512) 
+udp = Interferometry(11, './img/orion2.jpg', 512) 
 #udp = Interferometry(5, './img/orion.jpg', 32)
 
 def archipelago():    
@@ -30,11 +32,20 @@ def archipelago():
     archi.evolve()
     archi.wait_check()
     print(f'archi: {time() - t:0.3f}s')
+
+def check_good_solution():
+    fprob = single_objective(pg.problem(udp))
+    x = [-0.5525744962819084, -0.43883468127578895, -0.6785398184930235, -0.08114253160789063, -0.8411258059974988, 
+         -0.6056851931956526, -0.5663895555289944, 0.26568007012429795, -0.058986786386966486, -0.6311239695091586, 
+         -1.0, 0.9997028893794696, 0.9994590383759787, 0.9279325579572855, 0.981547346917406, 0.9999999999995614, 
+         0.9961605447057663, 0.9999999984827094, 0.9349754944331856, 0.676027417863097, 0.9998915752821322, 1.0]
+    y = fprob.fun(x)
+    print('fval = ' + str(y))
     
 def optimize():   
     fprob = single_objective(pg.problem(udp))
     print('interferometer de parallel function evaluation')
-    
+       
     # Python Differential Evolution implementation, uses ask/tell for parallel function evaluation.
     ret = de.minimize(fprob.fun, bounds=fprob.bounds, workers=16, popsize=32, max_evaluations=50000)
     
@@ -58,4 +69,5 @@ def optimize():
 if __name__ == '__main__':
     optimize()
     #archipelago()
+    #check_good_solution()
     pass
--- examples/interferometryudp.py
+++ examples/interferometryudp.py
@@ -5,43 +5,19 @@
 # https://repo.anaconda.com/archive/ using Python 3.8 on Linux
 # The test image used is here: https://api.optimize.esa.int/data/interferometry/orion.jpg
 
-from numba import njit
-from skimage.metrics import mean_squared_error
-import numpy as np
-from scipy import fft
-from PIL import Image
-import multiprocessing as mp
+import io
 import math
-import ctypes as ct
 from time import time
 
-# this is not how you should use numba, kills the CPU cache!!!!!!!!!!!!!!!!!!
-@njit(fastmath=True)
-def _get_observed_slow(n_points, im_ft, chromosome):
-    r, c = im_ft.shape
-    l = 0.01
-    
-    x, y = chromosome[:n_points], chromosome[n_points:]
-    
-    lx = (np.expand_dims(x, -1) - x).ravel()
-    ly = (np.expand_dims(y, -1) - y).ravel()
-    
-    theta = np.linspace(0, 2*np.pi, 10000)
-    cos_theta = np.cos(np.expand_dims(theta, -1))
-    sin_theta = np.sin(np.expand_dims(theta, -1))
-    
-    full_re_u = ((lx *  cos_theta + ly * sin_theta ) / l).astype(np.int32)
-    full_re_v = ((lx * -sin_theta + ly * cos_theta ) / l).astype(np.int32)    
-    
-    full_re_uimg = (full_re_u * r / 2**2.5 * l).astype(np.int32)
-    full_re_vimg = (full_re_v * r / 2**2.5 * l).astype(np.int32)
-    
-    obs_uv_matrix = np.zeros((r, c), dtype=np.int32)
-    for x, y in zip(full_re_uimg.flat, full_re_vimg.flat):
-        obs_uv_matrix[x][y] = 1
-        
-    return im_ft * obs_uv_matrix
+from matplotlib import pyplot
+from numba import njit
+from scipy import fft
+from skimage.metrics import mean_squared_error
+from skimage.transform import rescale, resize, downscale_local_mean
 
+import ctypes as ct
+import multiprocessing as mp
+import numpy as np
 
 @njit(fastmath=True)
 def _get_observed(n_points, im_ft, chromosome):
@@ -68,7 +44,6 @@ def _get_observed(n_points, im_ft, chromosome):
             
     return im_ft * obs_uv_matrix
 
-
 best = mp.RawValue(ct.c_double, math.inf) 
 count = mp.RawValue(ct.c_int, 0) 
 t0 = time()
@@ -84,8 +59,8 @@ class Interferometry():
         self.image_size_log2 = np.log2(image_size)
         self.image_size = image_size
 
-        img = Image.open(self.image)
-        img = img.resize((self.image_size,self.image_size))
+        img = pyplot.imread(self.image)
+        img = resize(img, (self.image_size,self.image_size), preserve_range=True, anti_aliasing=True)
 
         self.im_numpy = np.asarray(img)
         self.im_fft = fft.fft2(self.im_numpy)
@@ -94,7 +69,7 @@ class Interferometry():
         return ([-1.0]*self.number_points*2, [1.0]*self.number_points*2)
 
     def fitness(self, x):
-        observed = _get_observed(self.number_points, self.im_fft, x)
+        observed = _get_observed(self.number_points, self.im_fft, x)        
         im_reconstruct = fft.ifft2(observed).real
         val = (mean_squared_error(self.im_numpy, im_reconstruct),)
         with monitor:
