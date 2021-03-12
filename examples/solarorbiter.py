e313571521dce05cd6d70e1a0f3eb89d74007475
dietmarwo <drdietmarwolz@gmail.com>
2021-03-12 13:58:08 +0100

    v1.2.16

--- examples/solarorbiter.py
+++ examples/solarorbiter.py
@@ -1,101 +1,62 @@
-# requires pykep PR branch for 
-# https://github.com/esa/pykep/pull/127
+# This code is derived from https://github.com/esa/pykep/pull/127 
+# originally developed by Moritz v. Looz @mlooz . 
+# It was modified following suggestions from Waldemar Martens @MartensWaldemar_gitlab
 
-from math import acos
+# Solar orbiter is quite a challenge for state of the art optimizers, but
+# good solutions fulfilling the requirements can be found and an example is
+# shown in check_good_solution()
+#
+# See https://www.esa.int/Science_Exploration/Space_Science/Solar_Orbiter
+
 import time
-
-from numpy import sign
-from numpy.linalg import norm
-from pykep import AU, epoch
 from pykep.planet import jpl_lp
-from pykep.trajopt.gym._solar_orbiter import _solar_orbiter_udp
-
+from solar_orbiter_udp import _solar_orbiter_udp
 import matplotlib.pyplot as plt
+
 import pygmo as pg
-from fcmaes.advretry import minimize
-from fcmaes.optimizer import logger, de_cma, single_objective
-    
-#log to file and stdout
-logger('solarorbiter.log')
 
-tmin = epoch(time.time() / (24*3600) - 30*365 -7 + 2/24 - 2*365)
-tmax = epoch(time.time() / (24*3600) - 30*365 -7 + 2/24 + 2*365)
-
-def compute_solar_orbiter():
+from fcmaes import retry, advretry 
+from fcmaes.optimizer import logger, de_cma, single_objective, de, Bite_cpp
     
+# Other imports
+tmin = time.time() / (24*3600) - 30*365 -7 + 2/24 - 2*365
+tmax = time.time() / (24*3600) - 30*365 -7 + 2/24 + 2*365
+
+logger("solarorbiter.log")
+
+def check_good_solution():
     earth = jpl_lp("earth")
     venus = jpl_lp("venus")
-    seq = [earth, venus, venus, earth, venus, venus, venus, venus, venus]
- 
-    solar_orbiter = _solar_orbiter_udp([tmin, tmax], seq=seq)
-    
-    # Include delta v, mass and sun distance constraints
-    prob = pg.unconstrain(solar_orbiter,method="weighted",weights=[1.0, 10.0, 100, 100])
-    
-    fprob = single_objective(pg.problem(prob))
-    
-    #we replace pagmo pg.sade by fcmaes smart retry:
+    seq = [earth, venus, venus, earth, venus, venus, venus, venus, venus, venus]
+    solar_orbiter = _solar_orbiter_udp([tmin, tmax], seq=seq)  
+    prob = pg.problem(solar_orbiter)
+    x = [7454.820505282011, 399.5883816298621, 161.3293044402143, 336.35353340379817, 0.16706526043179085, -2.926263900573538, 
+         2.1707384653871475, 3.068749728236526, 2.6458336313296913, 3.0472278514692377, 2.426804445518446]  
+    print (prob.fitness(x)) 
+    solar_orbiter.pretty(x)
+    solar_orbiter.plot(x)
+    solar_orbiter.plot_distance_and_flybys(x)
      
-    logger().info('solar orbiter' + ' de -> cmaes c++ smart retry')
-    ret = minimize(fprob.fun, bounds=fprob.bounds, num_retries = 3000, 
-        logger = logger(), optimizer=de_cma(1500))
-    pop_champion_x = ret.x
-        
-    solar_orbiter.pretty(pop_champion_x)
-    solar_orbiter.plot(pop_champion_x)
-    
-    # Plot solar distance in AE
-    timeframe = range(1,5*365)
+def optimize():   
     earth = jpl_lp("earth")
     venus = jpl_lp("venus")
+    seq = [earth, venus, venus, earth, venus, venus, venus, venus, venus, venus]
+    solar_orbiter = _solar_orbiter_udp([tmin, tmax], seq=seq)  
+    prob = pg.problem(solar_orbiter)
+    fprob = single_objective(prob)   
+       
+    # logger().info('solar orbiter' + ' de -> cmaes c++ smart retry')
+    # ret = advretry.minimize(fprob.fun, bounds=fprob.bounds, num_retries = 3000, 
+        # logger = logger(), optimizer=de_cma(1500))
     
-    distances = []
-    edistances = []
-    vdistances = []
-    
-    for i in timeframe:
-        epoch = pop_champion_x[0]+i
-        pos, vel = solar_orbiter.eph(pop_champion_x, epoch)
-        epos, evel = earth.eph(epoch)
-        vpos, vvel = venus.eph(epoch)
-        distances.append(norm(pos) / AU)
-        edistances.append(norm(epos) / AU)
-        vdistances.append(norm(vpos) / AU)
-    
-    fig, ax = plt.subplots()
-    ax.plot(list(timeframe), distances, label="Solar Orbiter")
-    ax.plot(list(timeframe), edistances, label="Earth")
-    ax.plot(list(timeframe), vdistances, label="Venus")
-    ax.set_xlabel("Days")
-    ax.set_ylabel("AU")
-    ax.set_title("Distance to Sun")
-    ax.legend()
-    
-    # Plot inclination and distance
-    inclinations = []
-    for i in timeframe:
-        epoch = pop_champion_x[0]+i
-        pos, _ = solar_orbiter.eph(pop_champion_x, epoch)
-        inclination = sign(pos[2])*acos(norm(pos[:2]) / norm(pos))
-        inclinations.append(inclination)
-    
-    color = 'tab:red'
-    fig2, ax2 = plt.subplots()
-    ax2.plot(list(timeframe), inclinations, color=color)
-    ax2.set_ylabel("Inclination", color=color)
-    ax2.set_xlabel("Days")
-    ax.set_title("Distance and Inclination")
-    
-    ax3 = ax2.twinx()  # instantiate a second axes that shares the same x-axis
-    
-    color = 'tab:blue'
-    ax3.set_ylabel('AU', color=color)
-    ax3.plot(list(timeframe), distances, color=color)
-    ax3.tick_params(axis='y', labelcolor=color)
-    
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    plt.show()
-    
+    logger().info('solar orbiter' + ' BiteOpt parallel retry')
+    ret = retry.minimize(fprob.fun, bounds=fprob.bounds, num_retries = 32000, 
+                         logger = logger(), optimizer=Bite_cpp(100000, M=6))
+    return ret
+
 if __name__ == '__main__':
-    compute_solar_orbiter()
+    # optimize()
+    check_good_solution()   
+    plt.show()
+
     pass
