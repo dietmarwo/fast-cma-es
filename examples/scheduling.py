6972d1828217a6c73cf3b0381dba3a59fca906e0
dietmarwo <drdietmarwolz@gmail.com>
2022-01-04 21:52:23 +0100

    v1.3.11

--- examples/scheduling.py
+++ examples/scheduling.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory.
 
+# See https://github.com/dietmarwo/fast-cma-es/blob/master/tutorials/Scheduling.adoc for a detailed description.
+
 import math
 import pandas as pd
 import numpy as np
