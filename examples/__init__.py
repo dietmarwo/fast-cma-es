de57e9c87b222caedf016ea72bf542eb6e40975c
dietmarwo <drdietmarwolz@gmail.com>
2022-05-12 08:48:39 +0200

    v1.3.25

---
+++ examples/__init__.py
@@ -0,0 +1,38 @@
+# Copyright (c) Dietmar Wolz.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory.
+
+__version__ = '1.3.25'
+
+__all__ = [
+    'advexamples'
+    'benchmark_gtop'
+    'benchmark_gtop_pygmo'
+    'cassini_minlp'
+    'crypto'
+    'examples'
+    'f8'
+    'filter'
+    'gear_train'
+    'harvesting'
+    'house'
+    'interferometry'
+    'interferometry_udp'
+    'jobshop'
+    'lotka'
+    'moexamples'
+    'noisy_tsp'
+    'reproblem'
+    'repython'
+    'robot'
+    'rover'
+    'scheduling'
+    'solo_mgar'
+    'solo_mgar_udp'
+    'spring'
+    'tandem_minlp'
+    'top_trumps'
+    'tutorial'
+    'vessel'
+]
