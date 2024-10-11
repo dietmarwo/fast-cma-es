50df755e4d94bd6c8f5d1ff531229b678e1e73af
dietmarwo <drdietmarwolz@gmail.com>
2024-10-11 21:13:49 +0200

    v1.6.9

--- examples/lichtman/predict.py
+++ examples/lichtman/predict.py
@@ -27,7 +27,7 @@ a) It is predicted that Harris wins 2024
 and other weights + limits, so that 
 b) It is predicted that Trump wins 2024
 
-Executing the code below we get:
+Executing test_harris_prediciton() we get:
 
 Harris wins
 
@@ -75,6 +75,46 @@ Remarks:
   keep minting new keys until he happens to open all 38 doors". This example shows that you don't need to invent new keys
   to be a "locksmith" opening all doors, including future ones, predicting exactly what you want according to 
   your political agenda. 
+
+-----------------------------------------------------------------------------------------------------------
+  
+Is there a way we can measure the "robustness" of a model? We could use an epsilon value defining a
+limit area [limit-eps, limit+eps] and could maximize this epsilon still maintaining all predictions correct. 
+
+Executing test_harris_prediciton_eps() we get:
+
+Harris wins
+num correct 42.0 eps 1.968 weights [1.705, 2.095, 1.246, 0.012, 2.675, 0.009, 0.802, 0.811, 0.623, 1.664, 0.027, 0.004, 1.326] limit 12.95
+num correct 42.0 eps 1.926 weights [1.684, 2.064, 1.307, 0.078, 2.102, 0.011, 0.777, 0.937, 1.311, 1.631, 0.023, 0.002, 1.072] limit 12.998
+num correct 42.0 eps 1.878 weights [1.433, 2.239, 1.176, 0.001, 2.308, 0.004, 0.94, 0.961, 1.256, 1.981, 0.001, 0.007, 0.695] limit 12.967
+num correct 42.0 eps 1.983 weights [1.873, 2.056, 1.26, 0.011, 2.142, 0.002, 0.744, 0.631, 1.323, 1.5, 0.058, 0.013, 1.387] limit 12.976
+num correct 42.0 eps 1.885 weights [1.788, 2.047, 1.34, 0.132, 2.302, 0.009, 0.656, 0.878, 1.279, 1.478, 0.005, 0.008, 1.079] limit 12.935
+num correct 42.0 eps 1.826 weights [1.858, 1.925, 1.166, 0.014, 1.919, 0.001, 0.792, 0.784, 1.481, 1.576, 0.004, 0.003, 1.476] limit 12.98
+num correct 42.0 eps 1.893 weights [1.775, 2.151, 1.229, 0.022, 2.058, 0.021, 0.851, 0.752, 1.272, 1.6, 0.013, 0.004, 1.25] limit 12.968
+num correct 42.0 eps 1.67 weights [0.84, 1.897, 1.548, 0.499, 2.411, 0.008, 1.281, 1.278, 0.596, 2.126, 0.031, 0.025, 0.46] limit 12.983
+num correct 42.0 eps 2.007 weights [1.916, 2.124, 1.26, 0.006, 1.991, 0.003, 0.812, 0.642, 1.309, 1.474, 0.01, 0.003, 1.45] limit 12.997
+num correct 42.0 eps 2.019 weights [1.972, 1.922, 1.46, 0.162, 2.392, 0.02, 0.747, 0.612, 1.173, 1.235, 0.011, 0.0, 1.294] limit 12.993
+Harris looses
+num correct 42.0 eps 3.487 weights [1.282, 2.84, 0.097, 0.521, 2.858, 0.545, 0.881, 0.773, 1.126, 0.589, 0.002, 0.002, 1.485] limit 12.992
+num correct 42.0 eps 3.611 weights [1.087, 2.783, 0.082, 0.623, 2.718, 0.537, 1.161, 0.626, 1.169, 0.554, 0.01, 0.002, 1.647] limit 12.94
+num correct 42.0 eps 3.659 weights [1.181, 2.816, 0.066, 0.584, 3.969, 0.002, 1.104, 0.591, 0.665, 0.329, 0.002, 0.005, 1.689] limit 12.997
+num correct 42.0 eps 3.648 weights [1.15, 2.864, 0.012, 0.58, 5.374, 0.147, 1.145, 0.556, 0.895, 0.206, 0.006, 0.003, 0.061] limit 12.987
+num correct 42.0 eps 3.562 weights [0.907, 2.871, 0.003, 0.659, 2.88, 0.007, 1.307, 0.66, 1.104, 0.895, 0.018, 0.002, 1.688] limit 12.937
+num correct 42.0 eps 3.511 weights [0.898, 2.881, 0.005, 0.67, 2.905, 0.417, 1.313, 0.662, 1.295, 0.683, 0.005, 0.018, 1.247] limit 12.919
+num correct 42.0 eps 3.614 weights [1.088, 2.824, 0.074, 0.638, 2.898, 0.003, 1.161, 0.645, 1.033, 0.876, 0.001, 0.005, 1.754] limit 12.999
+num correct 42.0 eps 3.586 weights [1.205, 2.647, 0.244, 0.626, 2.948, 0.621, 1.04, 0.636, 0.995, 0.337, 0.013, 0.006, 1.682] limit 12.998
+num correct 42.0 eps 3.593 weights [1.117, 2.846, 0.018, 0.61, 3.593, 0.248, 1.115, 0.528, 1.131, 0.74, 0.022, 0.021, 1.01] limit 12.999
+num correct 42.0 eps 3.564 weights [1.12, 2.859, 0.015, 0.612, 2.96, 0.06, 1.103, 0.56, 0.883, 1.021, 0.015, 0.026, 1.766] limit 12.997
+
+Still all 42 predictions were correct in both cases, even when we change the last prediction for 2024. 
+
+Remarks: 
+
+- Now it becomes clear that keys 11 and 12 (and key 6 in the Harris wins case) are almost irrelevant.
+
+- Surprisingly the "Harris looses" prediction leads to a much larger epsilon value, so this prediction seems more robust. 
+  This result clearly contradicts Lichtmanns original prediction: Harris wins. 
+
 '''
 
 import numpy as np
@@ -131,6 +171,7 @@ def collect_all_data():
             print("error, sum props", sum(props), "incumbant is winner", inc_is_winner, file=sys.stderr)
     return data
 
+# optimize the correct predictions for a given model defined by its weights + limit
 def fit(n, props, inc_is_winner, x):
     weights = x[:-1]
     limit = x[-1]
@@ -139,6 +180,7 @@ def fit(n, props, inc_is_winner, x):
     num_correct = sum( [ inc_is_winner[i] == (sum(weighted_props[i]) > limit) for i in range(n)] )
     return -num_correct
 
+# show 10 solution models for "Harris wins" and 10 for "Harris looses"
 def find_other_model(data, harris_predict = True):
     
     props = np.array([d[0] for d in data])
@@ -161,10 +203,59 @@ def find_other_model(data, harris_predict = True):
         weights *= num_props/sum(weights) # normalize weights so that sum is num_props
         print ("num correct", -ret.fun, "weights", rl(list(weights)), "limit", r(limit))
 
-if __name__ == '__main__':
+def test_harris_prediciton():
     data = collect_all_data()
     print("Harris wins")
     find_other_model(data, harris_predict = True) # Harris wins
     print("Harris looses")
     find_other_model(data, harris_predict = False) # Harris looses
+
+
+# optimize the correct predictions for a given model defined by its weights + limit
+# maximize an epsilon around the limit to improve robustness
+def fit_eps(n, props, inc_is_winner, x):
+    weights = x[:-2]
+    eps = x[-1]
+    limit = x[-2]
+    weights *= len(props)/sum(weights) # normalize weights so that sum is num_props
+    weighted_props = weights*props
+    num_correct1 = sum( [ inc_is_winner[i] == (sum(weighted_props[i]) > limit + eps) for i in range(n)] )
+    num_correct2 = sum( [ (not inc_is_winner[i]) == (sum(weighted_props[i]) < limit - eps) for i in range(n)] )
+    return - (num_correct1 + num_correct2 + 0.1*eps)
+
+# show 10 solution models for "Harris wins" and 10 for "Harris looses" thereby maximizing epsilon
+def find_other_model_eps(data, harris_predict = True):
+    
+    props = np.array([d[0] for d in data])
+    inc_is_winner = np.array([d[1] for d in data])
+    # set for harris - last prediction
+    inc_is_winner[-1] = harris_predict
+    
+    n = len(data)
+    num_props = 13
+    dim = num_props + 2
+    lb = [0.0]*dim
+    ub = [1.0]*num_props + [13.0] + [4.999]
+    bounds = Bounds(lb,ub)
+       
+    for i in range(10):
+        ret = retry.minimize(partial(fit_eps, n, props, inc_is_winner), bounds, optimizer=Bite_cpp(5000),
+                   num_retries=32)
+        weights = ret.x[:-2]
+        limit = ret.x[-2]
+        eps = ret.x[-1]
+        weights *= num_props/sum(weights) # normalize weights so that sum is num_props
+        corr = int(-ret.fun) / 2
+        print ("num correct", corr, "eps", r(eps), "weights", rl(list(weights)), "limit", r(limit))
+
+def test_harris_prediciton_eps():
+    data = collect_all_data()
+    print("Harris wins")
+    find_other_model_eps(data, harris_predict = True) # Harris wins
+    print("Harris looses")
+    find_other_model_eps(data, harris_predict = False) # Harris looses
+
+if __name__ == '__main__':
+    test_harris_prediciton()
+    test_harris_prediciton_eps()
     pass
